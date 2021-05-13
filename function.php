99edbaf53cd97ad91031d3d954e83ae4a750c1a7
Kyosuke-Fukui <fs72os.kyosuke@gmail.com>
2021-05-13 22:02:42 +0900

    first commit

---
+++ function.php
@@ -0,0 +1,122 @@
+<?php 
+ 
+ //手札を数字の強い順に並べる関数
+function sort_hands($array){
+    $sort_keys = [];
+    foreach($array as $key => $value){
+        $sort_keys[$key] = $value['key'];
+    }
+    array_multisort($sort_keys,SORT_DESC,$array);
+    return $array;
+}   
+
+//配列で全ての組合せを返す関数
+function combination(array $arr, int $r): ?array
+{
+    // 重複した値を削除して，数値添字配列にする
+    // $arr = array_values(array_unique($arr));
+
+    $n = count($arr);
+    $result = []; // 最終的に二次元配列にして返す
+
+    // nCr の条件に一致していなければ null を返す
+    if($r < 0 || $n < $r){ return null; }
+
+    if($r === 1){
+        foreach($arr as $item){
+            $result[] = [$item];
+        }
+    }
+
+    if($r > 1){
+        // n - r + 1 回ループ
+        for($i = 0; $i < $n-$r+1; $i++){
+            // $sliced は $i + 1 番目から末尾までの要素
+            $sliced = array_slice($arr, $i + 1);
+            // 再帰処理 二次元配列が返ってくる
+            $recursion = combination($sliced, $r - 1);
+            foreach($recursion as $one_set){
+                array_unshift($one_set, $arr[$i]);
+                $result[] = $one_set;
+            }
+        }
+    }
+
+    return $result;
+}
+
+//役の判定
+function show_hands($array){
+    //スーツが同じ手札の枚数を数える
+    $suit_array = [];
+    foreach($array as $key => $value){
+        $suit_array[$key] = $value['suit'];
+    }
+    $same_suits = array_count_values($suit_array);
+    $count_same_suits = max($same_suits);
+
+    //数字が同じ手札の枚数を数える
+    $face_array = [];
+    foreach($array as $key => $value){
+        $face_array[$key] = $value['key'];
+    }
+    $same_faces = array_count_values($face_array);
+    $count_same_faces = max($same_faces);
+
+    //数字に重複がある場合の重複枚数が最も多いペアの数字
+    $pair;
+    // echo $face_array[0];
+    arsort($same_faces);
+    // var_dump($same_faces);
+    $pair =  array_keys($same_faces)[0];
+
+    $hand = [[],[],[]]; //役の名前、役の強さ、同じ役の場合の高い数字
+    //ストレート系判定
+    if($count_same_faces==1 && ($face_array[0]-1 == $face_array[1] && $face_array[1]-1 == $face_array[2] && $face_array[2]-1 == $face_array[3] && $face_array[3]-1 == $face_array[4])){
+        if($count_same_suits == 5){
+            if($face_array[0] == 12){
+                $hand = ["ロイヤルストレートフラッシュ",1,12];
+            }else {
+                $hand = ["ストレートフラッシュ",2,$face_array[0]];
+            }
+        }else {
+            $hand = ["ストレート",6,$face_array[0]];
+        }
+    }else {
+        if($count_same_suits == 5) $hand = ["フラッシュ",5,$face_array[0]];
+        if($count_same_faces == 4) $hand = ["フォーカード",3,$pair];
+        if ($count_same_faces == 3){
+            if(count($same_faces) == 2) {
+            $hand = ["フルハウス",4,$pair];
+            }else {
+            $hand = ["スリーカード",7,$pair];
+            }
+        } 
+        if($count_same_faces == 2) {
+            if(count($same_faces) == 3){
+            $hand = ["ツーペア",8,$pair];
+            }else{
+            $hand = ["ワンペア",9,$pair];
+            }
+        }
+        if($count_same_faces == 1) $hand = ["ハイカード",10,$face_array[0]];
+    }
+
+    return $hand;
+}
+
+//21通り（7C5）の組合せを、役の強さ順に並べる関数
+function max_hands($array){
+    //役のランク
+    $hand_rank = [];
+    foreach($array as $key => $value){
+        $hand_rank[$key] = $value[1];
+    }
+    //役が同じ時の数字のランク
+    $num_rank = [];
+    foreach($array as $key => $value){
+        $num_rank[$key] = $value[2];
+    }
+    array_multisort($hand_rank,SORT_ASC,$num_rank,SORT_DESC,$array);
+    return $array;
+}
---
+++ index.php
@@ -0,0 +1,140 @@
+<?php
+
+//デッキの作成
+$suits = ['♠','♥','♦','♣'];
+
+$faces = [];
+
+for($i = 2; $i<11; $i++){
+    $faces[]=$i;
+}
+$faces[]='J';
+$faces[]='Q';
+$faces[]='K';
+$faces[]='A';
+
+$deck = [];
+
+foreach($suits as $suit){
+    foreach($faces as $key => $face){
+        $deck[] = array("key"=>$key,"face"=>$face,"suit"=>$suit);
+    }
+}
+
+//手札の決定
+shuffle($deck);
+$cardPlayer = [];
+$cardOpp = [];
+$ComCard = [];
+
+for($i = 0; $i < 2; $i++) {
+    $cardPlayer[] = array_shift($deck);
+}
+
+for($i = 0; $i < 2; $i++) {
+    $cardOpp[] = array_shift($deck);
+}
+
+for($i = 0; $i < 5; $i++) {
+    $ComCard[] = array_shift($deck);
+}
+
+include './function.php';
+
+$cardPlayer_target = array_merge($cardPlayer,$ComCard);
+$cardOpp_target = array_merge($cardOpp,$ComCard);
+$cardPlayer_target_combi = combination($cardPlayer_target,5);
+$cardOpp_target_combi = combination($cardOpp_target,5);
+
+$cardPlayer_hand_combi = [];
+for ($i=0; $i < count($cardPlayer_target_combi); $i++) { 
+   $cardPlayer_hand_combi[] = show_hands(sort_hands($cardPlayer_target_combi[$i]));
+}
+
+$playerHand = max_hands($cardPlayer_hand_combi)[0];
+
+$cardOpp_hand_combi = [];
+for ($i=0; $i < count($cardOpp_target_combi); $i++) { 
+   $cardOpp_hand_combi[] = show_hands(sort_hands($cardOpp_target_combi[$i]));
+}
+
+$oppHand = max_hands($cardOpp_hand_combi)[0];
+
+$messege;
+//勝敗判定
+if($playerHand[1] < $oppHand[1]){
+    $messege = 'あなたの勝ちです！！';
+}elseif($playerHand[1] > $oppHand[1]){
+    $messege = 'あなたの負けです＞＜';
+}else{
+    if($playerHand[2] > $oppHand[2]){
+    $messege = 'あなたの勝ちです！！';
+    }elseif($playerHand[2] < $oppHand[2]){
+    $messege = 'あなたの負けです＞＜';
+    }else{
+    $messege = '引き分けです';
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>シンプルホールデム</title>
+    <style>
+    h1{
+        text-align:center;
+    }
+
+    .main{
+        width: 400px; margin: 80px auto; text-align:center;
+    }
+    .result{
+        margin:50px 0 
+    }
+    </style>
+</head>
+<body>
+    <h1>シンプルホールデム</h1>
+    <div class="main">
+        <div>
+            <?php 
+            echo <<<EOM
+            場のカード：
+            {$ComCard[0]['face']}<span>{$ComCard[0]['suit']}</span>
+            {$ComCard[1]['face']}<span>{$ComCard[1]['suit']}</span>
+            {$ComCard[2]['face']}<span>{$ComCard[2]['suit']}</span>
+            {$ComCard[3]['face']}<span>{$ComCard[3]['suit']}</span>
+            {$ComCard[4]['face']}<span>{$ComCard[4]['suit']}</span><br><br><br>
+            相手の手札：
+            {$cardOpp[0]['face']}<span>{$cardOpp[0]['suit']}</span>
+            {$cardOpp[1]['face']}<span>{$cardOpp[1]['suit']}</span><br><br>
+            「{$oppHand[0]}」<br><br>
+            自分の手札：
+            {$cardPlayer[0]['face']}<span>{$cardPlayer[0]['suit']}</span>
+            {$cardPlayer[1]['face']}<span>{$cardPlayer[1]['suit']}</span><br><br>
+            「{$playerHand[0]}」
+            EOM;
+            ?>
+        </div>
+        <div class="result">
+           <h2><?php echo $messege; ?></h2>
+        </div>
+
+        <input type="button"  value="もう一度遊ぶ" onclick="koshin()">
+    
+    </div>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+    <script>
+    function koshin(){
+        location.reload();
+    }
+
+    $("span:contains('♥')").css("color","red");
+    $("span:contains('♦')").css("color","red");
+    </script>
+</body>
+</html>
